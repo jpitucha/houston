e5ff8eae2b2499ed6232a8773bf3fd60d3382df1
Jakub Pitucha <vaguevans@gmail.com>
2020-12-06 13:18:56 +0100

    corrected intendation and naming

--- api/n2yo/index.js
+++ api/n2yo/index.js
@@ -5,7 +5,7 @@ class N2YO {
 
     static #apiAddress = 'https://api.n2yo.com/rest/v1/satellite'
 
-    static getTle(id) {
+    static getTwoLineElements(id) {
         const endPoint = `${this.#apiAddress}/tle/${id}&apiKey=${process.env.N2YO_KEY}`
         
         return fetch(endPoint)
--- routes/local.js
+++ routes/local.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const n2yo = require('./../api/n2yo')
 
-router.get('/tle/:id', (req, res) => {
-    n2yo.getTle(req.params.id)
-    .then(data => res.json(data))
+router.get('/two-line-elements/:id', (req, res) => {
+    n2yo.getTwoLineElements(req.params.id)
+        .then(data => res.json(data))
 })
 
 module.exports = router
