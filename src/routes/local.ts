9b4993b638462f9d37641902a3c981af63cdba56
Jakub Pitucha <vaguevans@gmail.com>
2020-12-08 20:02:53 +0100

    added type guard

--- src/routes/local.ts
+++ src/routes/local.ts
@@ -4,6 +4,7 @@ import n2yo from './../api/n2yo/index.js'
 const router = express.Router()
 
 router.get('/two-line-elements/:id', (req, res) => {
+    if (req.params.id == undefined) return res.sendStatus(400)
     n2yo.getTwoLineElements(req.params.id)
         .then(result => res.json(result))
         .catch(() => res.sendStatus(400))
