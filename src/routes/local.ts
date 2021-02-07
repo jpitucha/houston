f11e473acb0f7e0dea3d58877cd6d948f2e2ffbd
Jakub Pitucha <vaguevans@gmail.com>
2021-02-07 21:04:01 +0100

    added middleware

--- src/routes/local.ts
+++ src/routes/local.ts
@@ -3,6 +3,7 @@ import n2yo from "./../api/n2yo/index.js";
 import SatelliteUtilities from "../utils/satelliteUtils";
 import _ from "lodash";
 import SatelliteResponseInterface from '../utils/types/parialSatelliteInterface'
+import { assert, pattern, string } from 'superstruct'
 
 const router = express.Router();
 const PROPS_TO_SEND_IN_RESPOSNE = [
@@ -15,41 +16,74 @@ const PROPS_TO_SEND_IN_RESPOSNE = [
   "purpose",
 ];
 
+router.get("/two-line-elements/:id", (req, res, next) => {
+  const id = req.params.id
+  const idCheck = pattern(string(), /[0-9]+/)
+
+  if (!id) return res.sendStatus(400)
+  try {
+    assert(id, idCheck)
+  } catch {
+    return res.sendStatus(400)
+  }
+  req.id = id
+  next()
+})
+
 router.get("/two-line-elements/:id", (req, res) => {
-  if (req.params.id === undefined) return res.sendStatus(400);
   n2yo
-    .getTwoLineElements(req.params.id)
+    .getTwoLineElements(req.id)
     .then((result) => res.json(result))
     .catch(() => res.sendStatus(400));
 });
 
-router.get("/satellite", (req, res) => {
-  if (typeof req.query.id === "string") {
-    return SatelliteUtilities.getSatelliteById(req.query.id)
-      .then((satelliteDoc) => {
-        if (!satelliteDoc) return res.sendStatus(400);
-        return res.json(_.pick(satelliteDoc, PROPS_TO_SEND_IN_RESPOSNE));
-      })
-      .catch(() => {
-        return res.sendStatus(400);
-      });
+router.get("/satellite/by-id/:id", (req, res, next) => {
+  const id = req.params.id
+  const idCheck = pattern(string(), /[0-9]+/)
+
+  if (!id) return res.sendStatus(400)
+  try {
+    assert(id, idCheck)
+  } catch {
+    return res.sendStatus(400)
   }
-  if (typeof req.query.name === "string") {
-    return SatelliteUtilities.getSatelliteByName(req.query.name)
-      .then((satelliteDoc) => {
-        if (!satelliteDoc) return res.sendStatus(400);
-        const satellitesArray = <SatelliteResponseInterface[]>(
-          satelliteDoc.map((satellite) =>
-            _.pick(satellite, PROPS_TO_SEND_IN_RESPOSNE)
-          )
-        );
-        return res.json(satellitesArray);
-      })
-      .catch(() => {
-        return res.sendStatus(400);
-      });
-  }
-  res.sendStatus(400);
-});
+  req.id = id
+  next()
+})
+
+router.get("/satellite/by-id/:id", (req, res) => {
+  return SatelliteUtilities.getSatelliteById(req.id)
+    .then((satelliteDoc) => {
+      if (!satelliteDoc) return res.sendStatus(400);
+      return res.json(_.pick(satelliteDoc, PROPS_TO_SEND_IN_RESPOSNE));
+    })
+    .catch(() => {
+      return res.sendStatus(400);
+    });
+})
+
+router.get("/satellite/by-name/:name", (req, res, next) => {
+  const name = req.params.name
+
+  if (!name) return res.sendStatus(400)
+  req.name = name
+  next()
+})
+
+router.get("/satellite/by-name/:name", (req, res) => {
+  return SatelliteUtilities.getSatelliteByName(req.name)
+    .then((satelliteDoc) => {
+      if (!satelliteDoc) return res.sendStatus(400);
+      const satellitesArray = <SatelliteResponseInterface[]>(
+        satelliteDoc.map((satellite) =>
+          _.pick(satellite, PROPS_TO_SEND_IN_RESPOSNE)
+        )
+      );
+      return res.json(satellitesArray);
+    })
+    .catch(() => {
+      return res.sendStatus(400);
+    });
+})
 
 export default router;
