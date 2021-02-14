d50fa4fa0c9d491d1b3df4db85e837dc4a009188
Jakub Pitucha <vaguevans@gmail.com>
2021-02-14 01:07:40 +0100

    hotfix

--- src/validators/satelliteNameValidator.ts
+++ src/validators/satelliteNameValidator.ts
@@ -1,7 +1,7 @@
 import { Request, Response, response } from 'express'
 
 export const satelliteNameRouteValidation = (req: Request, res: Response, next: CallableFunction): void | ReturnType<typeof response.sendStatus> => {
-    const name = req.params.officialName
+    const name = req.params.name
     if (!name) return res.sendStatus(400)
     return next()
 }
