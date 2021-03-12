d97ebcab933e456e3ba9b8b3f418e5f7b5f77609
Jakub Pitucha <vaguevans@gmail.com>
2021-03-12 22:01:33 +0100

    regexp fix

--- src/validators/satelliteIdValidator.ts
+++ src/validators/satelliteIdValidator.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express'
 import { assert, pattern, string } from 'superstruct'
 
 export const validateSatelliteIdRoute = (req: Request, res: Response, next: NextFunction): void => {
-    const idCheck = pattern(string(), /^[1-9][0-9]+$/)
+    const idCheck = pattern(string(), /^[1-9][0-9]*$/)
     try {
         assert(req.query.id, idCheck)
     } catch {
